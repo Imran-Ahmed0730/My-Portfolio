51406eff6452e84a9ab648f92b830a81c85d5640
Imran Ahmed <66079611+Imran-Ahmed0730@users.noreply.github.com>
2023-11-02 03:21:59 +0600

    Admin Panel Initialization
    1. Admin Panel Initialized.
    2. Performed Admin Panel Mastering

--- my_portfolio/resources/views/admin/master.blade.php
+++ my_portfolio/resources/views/admin/master.blade.php
@@ -1,1 +1,54 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from admin.pixelstrap.com/zeta/theme/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 01 Nov 2023 20:10:12 GMT -->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{asset('admin-assets')}}assets/images/logo/favicon-icon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('admin-assets')}}assets/images/logo/favicon-icon.png" type="image/x-icon">
+    <title>Zeta admin dashboard </title>
+    <!-- Google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap" rel="stylesheet">
+    @include('admin.include.style')
+</head>
+<body>
+<div class="loader-wrapper">
+    <div class="loader">
+        <div class="loader-bar"></div>
+        <div class="loader-bar"></div>
+        <div class="loader-bar"></div>
+        <div class="loader-bar"></div>
+        <div class="loader-bar"></div>
+        <div class="loader-ball"></div>
+    </div>
+</div>
+
+<div class="tap-top"><i data-feather="chevrons-up"></i></div>
+
+<div class="page-wrapper compact-wrapper" id="pageWrapper">
+    <!-- Page Header Start-->
+    @include('admin.include.header')
+
+    <div class="page-body-wrapper">
+
+        @include('admin.include.sidenavbar')
+        @yield('content')
+    </div>
+
+</div>
+
+<!-- footer start-->
+@include('admin.include.footer')
+</div>
+</div>
+<!-- latest jquery-->
+@include('admin.include.script')
+</body>
+
+</html>
